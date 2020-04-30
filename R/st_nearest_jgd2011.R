c2fd3f3b4891e2322eb2766568f8d859a79c1996
Shinya Uryu <suika1127@gmail.com>
2020-04-30 18:06:08 +0900

    Tweaks

--- R/st_nearest_jgd2011.R
+++ R/st_nearest_jgd2011.R
@@ -1,15 +1,16 @@
-#' Identify the coordinate system of the coordinates
-#' 
-#' @description Returns the value when the coordinates of ESPG:4326 given to the input 
+#' Identify the Japan plane rectangular CS
+#'
+#' @description Returns the value when the coordinates of ESPG:4326 given to the input
 #' are replaced with those of the Japan Plane Rectangular CS.
 #' @param geometry geometry (POINT)
 #' @examples
 #' library(sf)
 #' st_nearest_jgd2011(st_sfc(sf::st_point(c(140.778, 36.8)), crs = 4326))
+#' @seealso [https://www.gsi.go.jp/LAW/heimencho.html](https://www.gsi.go.jp/LAW/heimencho.html)
 #' @export
 st_nearest_jgd2011 <- function(geometry) {
   purrr::map_dbl(
-    sf::st_nearest_feature(geometry, 
+    sf::st_nearest_feature(geometry,
                            jgd2011_bbox),
     ~ as.numeric(as.character(jgd2011_bbox[[.x,
                                             "epsg"]])))
