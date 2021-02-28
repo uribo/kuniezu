0f03f0aac57ba80f817192333dfec989d06ef320
Shinya Uryu <uryu.shinya@nies.go.jp>
2021-02-28 10:33:53 +0900

    fix geos change
    ref #6

--- tests/testthat/test-move_jpn_rs.R
+++ tests/testthat/test-move_jpn_rs.R
@@ -1,5 +1,4 @@
 test_that("multiplication works", {
-  skip_on_cran()
   res <-
     move_jpn_rs(jgd2011_bbox)
   expect_equal(
@@ -9,11 +8,24 @@ test_that("multiplication works", {
     as.character(res$system),
     as.character(as.roman(seq_len(19)[-c(18, 19)]))
   )
-  expect_equal(
-    sf::st_as_text(res$geometry[17]),
-    sf::st_as_text(jgd2011_bbox$geometry[17] +
-                     c(5.2, 15.0))
-  )
+  x <-
+    jgd2011_bbox$geometry[17] + c(5.2, 15.0)
+  sf::st_crs(x) <- 6668
+  if (sf::sf_extSoftVersion()["GEOS"] < "3.9.0") {
+    expect_equal(
+      sf::st_as_text(res$geometry[17]),
+      sf::st_as_text(x)
+    )
+  } else {
+    expect_true(
+      sf::st_equals(res$geometry[17],
+                    x,
+                    sparse = FALSE))
+    expect_false(
+      sf::st_equals(res$geometry[1],
+                    x,
+                    sparse = FALSE))
+  }
   res <-
     move_jpn_rs(jgd2011_bbox, clip = FALSE)
   expect_equal(
