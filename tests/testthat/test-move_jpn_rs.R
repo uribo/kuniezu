e6e3233e4442134fd462760b66d56c5767b91271
Shinya Uryu <suika1127@gmail.com>
2023-01-10 13:56:30 +0900

    Update

--- tests/testthat/test-move_jpn_rs.R
+++ tests/testthat/test-move_jpn_rs.R
@@ -11,21 +11,6 @@ test_that("multiplication works", {
   x <-
     jgd2011_bbox$geometry[17] + c(5.2, 15.0)
   sf::st_crs(x) <- 6668
-  if (sf::sf_extSoftVersion()["GEOS"] < "3.9.0") {
-    expect_equal(
-      sf::st_as_text(res$geometry[17]),
-      sf::st_as_text(x)
-    )
-  } else {
-    expect_true(
-      sf::st_equals(res$geometry[17],
-                    x,
-                    sparse = FALSE))
-    expect_false(
-      sf::st_equals(res$geometry[1],
-                    x,
-                    sparse = FALSE))
-  }
   res <-
     move_jpn_rs(jgd2011_bbox, clip = FALSE)
   expect_equal(
